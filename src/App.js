e7e3bf3ad5fb91553feb969ac9fdb72859b8e4ac
Ritik Ranjan singh <94626673+Raone2728@users.noreply.github.com>
2023-04-22 22:32:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import List from './List';
+
+const App = () => {
+  const items = [
+    { text: 'Item 1' },
+    { text: 'Item 2' },
+    { text: 'Item 3' },
+     { text: 'Item 4' },
+  ];
+
+  return (
+    <div>
+      <List items={items} />
+    </div>
+  );
+};
+
+export default App;
